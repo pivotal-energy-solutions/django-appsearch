e30b61b88fd3df74e39e1e1eb3ace6cd4bd5f9df
Steven Klass <sklass@7stalks.com>
2013-01-04 11:27:04 -0700

    More cleanup removal of AxisTestingCore.  RIP.

--- appsearch/utils.py
+++ appsearch/utils.py
@@ -16,83 +16,83 @@ log = logging.getLogger(__name__)
 
 class Searcher(StrAndUnicode):
     """ Template helper, wrapping all the necessary components to render an appsearch page. """
-    
+
     # Methods and fields not meant to be accessed from the template should start with an underscore
     # to let the template variable name resolution block access.
-    
+
     model_selection_form = None
     constraint_formset = None
-    
+
     field_data_url = None
     operator_data_url = None
-    
+
     results = None
-    
+
     # Processing callback hooks
     _display_fields_callback = None
     _build_queryset_callback = None
     _process_results_callback = None
-    
+
     ## Fallback items normally provided by the view
     context_object_name = 'search'
-    
+
     # Default templates
     form_template_name = "appsearch/default_form.html"
     search_form_template_name = "appsearch/search_form.html"
     results_list_template_name = "appsearch/results_list.html"
-    
+
     def __init__(self, request, url=None, querydict=None, registry=search, **kwargs):
         self.kwargs = kwargs
         self.request = request
         self.url = url or request.path
         self._determine_urls(kwargs)
-        
+
         self._forms_ready = False
         self._set_up_forms(querydict or request.GET, registry)
-        
+
         # Fallback items
         self.context_object_name = kwargs.get('context_object_name', self.context_object_name)
         self.form_template_name = kwargs.get('form_template_name', self.form_template_name)
         self.search_form_template_name = kwargs.get('search_form_template_name', self.search_form_template_name)
         self.results_list_template_name = kwargs.get('results_list_template_name', self.results_list_template_name)
-        
+
         self._display_fields_callback = kwargs.get('display_fields_callback')
         self._build_queryset_callback = kwargs.get('build_queryset_callback')
         self._process_results_callback = kwargs.get('process_results_callback')
-    
+
     # Rendering methods
     def __unicode__(self):
         return render_to_string(self.form_template_name, RequestContext(self.request, {
             self.context_object_name: self,
         }))
-    
+
     def render_search_form(self):
         """ Renders only the template at ``search_form_template_name`` """
         return render_to_string(self.search_form_template_name, RequestContext(self.request, {
             self.context_object_name: self,
         }))
-    
+
     def render_results_list(self):
         """ Renders only the template at ``results_list_template_name`` """
         return render_to_string(self.results_list_template_name, RequestContext(self.request, {
             self.context_object_name: self,
         }))
-    
+
     @property
     def ready(self):
         """ Indicates if a search has been executed by the constructed state. """
-        
+
         if self._forms_ready:
             self.model_config = self.model_selection_form.get_selected_configuration()
             self.model = self.model_config.model
-        
+
         return self._forms_ready
-        
+
     def _set_up_forms(self, querydict, registry, permission=None):
         self.model_selection_form = ModelSelectionForm(registry, self.request.user, querydict,
                 permission=permission)
         constraint_formset_class = formset_factory(ConstraintForm, formset=ConstraintFormset)
-        
+
         if self.model_selection_form.is_valid():
             model_configuration = self.model_selection_form.get_selected_configuration()
             self.constraint_formset = constraint_formset_class(model_configuration, querydict)
@@ -102,15 +102,14 @@ class Searcher(StrAndUnicode):
             self.model_selection_form = ModelSelectionForm(registry, self.request.user,
                     permission=permission)
             self.constraint_formset = constraint_formset_class(configuration=None)
-    
+
     def _determine_urls(self, kwargs):
         # If a URL is not customized, this namespace will be used to search out the default URL
         url_namespace = kwargs.get('url_namespace')
-        
+
         # Check custom data URLs
         field_url = kwargs.get('field_url')
         operator_url = kwargs.get('operator_url')
-        
         if field_url is None:
             url_name = 'appsearch:constraint-fields'
             if url_namespace is not None:
@@ -121,68 +120,68 @@ class Searcher(StrAndUnicode):
             if url_namespace is not None:
                 url_name = url_namespace + ':' + url_name
             operator_url = reverse(url_name, kwargs=kwargs.get('operator_url_kwargs', {}))
-        
+
         self.field_data_url = field_url
         self.operator_data_url = operator_url
-    
+
     def _perform_search(self):
         """
         Generates the query using the validated constraint formset.  Also compiles a natural
         language string (``self.natural_string``) in the format of
         "N [Model]s with [field] [operator] '[term]'".
-        
+
         """
-        
+
         natural_string = []
-        
+
         # 2-tuples of an operator and Q instance, sent through reduce() after it's built
         query_list = []
-        
+
         for i, constraint_form in enumerate(self.constraint_formset):
             type_operator = constraint_form.cleaned_data['type']
             field_list = constraint_form.cleaned_data['field']
             constraint_operator = constraint_form.cleaned_data['operator']
             term = constraint_form.cleaned_data['term']
             end_term = constraint_form.cleaned_data['end_term']
-            
+
             verbose_name = self.model_config._fields[field_list]
-            
+
             # Build this field's query.
             # Search fields bound together in a tuple are considered OR conditions for a single
             # virtual field name.
             query = None
             for field in field_list:
                 value = term
-                
+
                 # Prep an inverted lookup
                 negative = constraint_operator.startswith('!')
                 if negative:
                     constraint_operator = constraint_operator[1:]
-                
+
                 if constraint_operator == "isnull":
                     value = not negative
                     negative = False
-                
+
                 # Bake the queryset language string
                 field_query = LOOKUP_SEP.join((field, constraint_operator))
-                
+
                 q = Q(**{
                     field_query: value,
                 })
-                
+
                 log.debug("Querying %s [%d]: %s=%r", self.model.__name__, i, field_query, value)
-                
+
                 # Negate if necessary
                 if negative:
                     q = ~q
-                
+
                 if query is None:
                     query = q
                 else:
                     query |= q
-                
+
             query_list.append((type_operator, query))
-            
+
             # Do some natural processing
             if isinstance(value, (tuple, list)):
                 value = ' - '.join(map(unicode, value))
@@ -192,25 +191,25 @@ class Searcher(StrAndUnicode):
             if i != 0: # Skip the leading "and" on the first constraint
                 bits.insert(0, constraint_form['type'].value())
             natural_string.append(bits)
-        
+
         # The first query's "type" should be ignored for the sake of the reduce line below.
         query = reduce(lambda q1, (op, q2): op(q1, q2), query_list[1:], query_list[0][1])
-        
+
         queryset = self.build_queryset(self.model, query)
         data_rows = self.process_results(queryset)
-        
+
         self.results = {
             'count': len(queryset),
             'list': data_rows,
             'fields': self._get_display_fields(),
             'natural_string': "where " + ', '.join(map(' '.join, natural_string)),
         }
-    
+
     def _get_display_fields(self, model, config):
         if self._display_fields_callback:
             return self._display_fields_callback(self, model, config)
         return config.get_display_fields()
-    
+
     def get_select_related_fields(self, model, config):
         """ Returns a list of queryset language names to pass into ``.select_related()`` """
         display_fields = self._get_display_fields(model, config)
@@ -220,54 +219,54 @@ class Searcher(StrAndUnicode):
                 _, field_name = field_info
             else:
                 field_name = field_info
-            
+
             related_path = field_name.rsplit(LOOKUP_SEP, 1)[0]
-            
+
             if LOOKUP_SEP in related_path:
                 related_names.add(related_path)
-        
+
         return related_names
-    
+
     def build_queryset(self, model, query, queryset=None):
         """
         Returns the queryset using ``query``.
-        
+
         Default behavior inspects the display fields for any related items and requests their
         selection and adds ``.distinct()``.
-        
+
         If ``base_queryset`` is provided, it will be used as the starting point for applying the
         ``query`` filter.  This is useful for subclasses that want to change the default manager
         being accessed for the initial queryset.  Passing up a ``queryset`` via super() will
         accomplish this, returning a fully built queryset with minimum hassle.
-        
+
         If ``build_queryset`` callback was defined on the originating view, it will be called
         after the queryset is built and will be sent the searcher instance, model, config, initial
         ``Q`` query, and the derived queryset.  The callback should return the queryset in its final
         state for execution.
-        
+
         """
-        
+
         related_names = self.get_select_related_fields(model, self.model_config)
-        
+
         if queryset is None:
             queryset = self.model_config.get_queryset(self.request.user)
-        
+
         queryset = queryset.filter(query).select_related(*related_names).distinct()
-        
+
         if self._build_queryset_callback:
             queryset = self._build_queryset_callback(self, model, self.model_config, query,
                     queryset)
-        
+
         return queryset
-    
+
     def process_results(self, queryset):
         """
         Should return a sequence of n-tuples, where ``n`` is the number of columns to be shown in
         the table.
-        
+
         """
-        
+
         if self._process_results_callback:
             return self._process_results_callback(self, self.model, self.model_config, queryset)
-        
+
         return [self.model_config.get_object_data(obj) for obj in queryset]
--- appsearch/views.py
+++ appsearch/views.py
@@ -9,55 +9,55 @@ from appsearch.registry import search
 
 class SearchMixin(object):
     searcher_class = Searcher
-    
+
     context_object_name = 'search'
     form_template_name = "appsearch/default_form.html"
     search_form_template_name = "appsearch/search_form.html"
     results_list_template_name = "appsearch/results_list.html"
-    
+
     # Callbacks, unprovided by default
     get_display_fields = None
     build_queryset = None
     process_results = None
-    
+
     def get_context_data(self, **kwargs):
         context = super(SearchMixin, self).get_context_data(**kwargs)
-        
+
         object_name = self.get_context_object_name()
         searcher = self.get_searcher()
-        
+
         if searcher.ready:
             searcher._perform_search()
-        
+
         context[object_name] = searcher
         return context
-    
+
     def get_searcher_class(self):
         """ Returns the view's ``searcher_class`` attribute. """
         return self.searcher_class
-    
+
     def get_searcher(self):
         """ Builds and returns a ``Searcher`` instance for this search context. """
         return self.get_searcher_class()(self.request, **self.get_searcher_kwargs())
-    
+
     def get_searcher_kwargs(self):
         """ Returns the dictionary of kwargs sent to the ``Searcher`` constructor. """
-        
+
         return {
             'form_template_name': self.get_form_template_name(),
             'search_form_template_name': self.get_search_form_template_name(),
             'results_list_template_name': self.get_results_list_template_name(),
             'context_object_name': self.get_context_object_name(),
-            
+
             # Callbacks
             'display_fields_callback': self.get_display_fields,
             'build_queryset_callback': self.build_queryset,
             'process_results_callback': self.process_results,
         }
-    
+
     def get_context_object_name(self):
         return self.context_object_name
-    
+
     def get_form_template_name(self):
         return self.form_template_name
 
@@ -72,29 +72,27 @@ class BaseSearchView(SearchMixin, TemplateView):
 
 
 class BaseAjaxConfigurationResolutionView(View):
+
     def dispatch(self, request, *args, **kwargs):
         self.request = request
         self.args = args
         self.kwargs = kwargs
-        
         model = kwargs.get('model', request.GET.get('model'))
-        
+
         config, response = self.resolve_configuration(request, model)
-        
+
         if config is None:
             return response
-        
+
         self.configuration = config
-        
+
         return super(BaseAjaxConfigurationResolutionView, self).dispatch(request, model=model)
-    
+
     def resolve_configuration(self, request, model):
         if not request.is_ajax() and request.GET.get('ajax') != 'true':
             return None, HttpResponseBadRequest("Bad Request")
-        
         if model is None:
             return None, HttpResponseBadRequest("No model is supplied")
-        
         try:
             content_type = ContentType.objects.get(id=model)
         except (ValueError, ContentType.DoesNotExist):
@@ -104,54 +102,52 @@ class BaseAjaxConfigurationResolutionView(View):
         else:
             model = '.'.join((content_type.app_label, content_type.model))
             model_class = content_type.model_class()
-        
+
         if model_class and self.has_perm(model_class) and model in search:
             configuration = search[model]
         else:
             configuration = None
-        
+
         if not configuration:
             return None, HttpResponseForbidden()
-        
+
         return configuration, None
-    
+
     def has_perm(self, model_class):
         permission = '{}.change_{}'.format(model_class._meta.app_label, model_class.__name__.lower())
         return self.request.user.has_perm(permission)
-    
-    
+
+
 class ConstraintFieldsAjaxView(BaseAjaxConfigurationResolutionView):
     """
     Looks up a model class based on the regex parameter "model" (or from the GET data), which is a
     valid ``ContentType`` pk.
-    
+
     """
-    
+
     def get(self, request, *args, **kwargs):
         data = {'choices': self.configuration.get_searchable_field_choices(include_types=True)}
-        
         return HttpResponse(json.dumps(data, indent=4), content_type='text/json')
-    
+
 class ConstraintOperatorsAjaxView(BaseAjaxConfigurationResolutionView):
     """
     Looks up a model class based on the regex parameter "model" (or from the GET data), which is a
     valid ``ContentType`` pk.
-    
+
     """
-    
+
     def get(self, request, *args, **kwargs):
         field = request.GET.get('field')
-        
         try:
             choices = self.configuration.get_operator_choices(hash=field, flat=True)
         except AttributeError:
             choices = None
-        
+
         if not choices:
             return HttpResponseBadRequest("Bad field")
-        
+
         data = {'choices': choices}
-        
+
         return HttpResponse(json.dumps(data, indent=4), content_type='text/json')
-    
+
 
