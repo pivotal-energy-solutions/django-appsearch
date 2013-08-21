e2c5846ba08735a5200a5b40d2d3745931ab04bd
Tim Valenta <tonightslastsong@gmail.com>
2013-08-20 17:06:58 -0700

    Allow objects without dedicated URLs to appear in results

--- appsearch/registry.py
+++ appsearch/registry.py
@@ -374,7 +374,8 @@ class ModelSearch(object):
             data.append(value)
 
         # Convert the first column's data into a link to the model instance
-        data[0] = u"""<a href="{}">{}</a>""".format(obj.get_absolute_url(), data[0])
+        if hasattr(obj, 'get_absolute_url'):
+            data[0] = u"""<a href="{}">{}</a>""".format(obj.get_absolute_url(), data[0])
 
         return data
 
