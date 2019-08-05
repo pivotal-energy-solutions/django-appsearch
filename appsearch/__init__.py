81c2c35370b1d6221670a6cb2fe963985b4b9cc1
Steven K <steven@pointcircle.com>
2019-08-05 15:15:26 -0700

    Version Bump

--- appsearch/__init__.py
+++ appsearch/__init__.py
@@ -5,7 +5,7 @@ import sys
 
 __name__ = 'appsearch'
 __author__ = 'Pivotal Energy Solutions'
-__version_info__ = (1, 1, 2)
+__version_info__ = (1, 1, 4)
 __version__ = '.'.join(map(str, __version_info__))
 __date__ = '2014/07/22 4:47:00 PM'
 __credits__ = ['Tim Valenta', "Steven Klass"]
