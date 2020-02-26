69094a9a174bcb989c6c7c2d11c9fd94583b6e5b
Autumn Valenta <avalenta@pivotalenergysolutions.com>
2020-02-26 15:36:10 -0700

    Fix __name__
    This must not change unless pip requirements elsewhere are updated to match the egg name.  If we want to strip off the `django-` prefix again later, we'll need to plan for it in the deploy process.

--- appsearch/__init__.py
+++ appsearch/__init__.py
@@ -5,7 +5,7 @@ import logging
 import sys
 
 
-__name__ = 'appsearch'
+__name__ = 'django-appsearch'
 __author__ = 'Pivotal Energy Solutions'
 __version_info__ = (1, 1, 6)
 __version__ = '.'.join(map(str, __version_info__))
