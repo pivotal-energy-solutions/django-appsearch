00f837144a6af1e1faab77be1e00de00ca0a6a4c
artemhruzd <arti3d.artem@gmail.com>
2020-02-28 13:04:14 +0530

    update user permission check for every model

--- appsearch/__init__.py
+++ appsearch/__init__.py
@@ -3,9 +3,10 @@
 
 import logging
 
+
 __name__ = 'appsearch'
 __author__ = 'Pivotal Energy Solutions'
-__version_info__ = (1, 1, 6)
+__version_info__ = (1, 1, 7)
 __version__ = '.'.join(map(str, __version_info__))
 __date__ = '2014/07/22 4:47:00 PM'
 __credits__ = ['Tim Valenta', "Steven Klass"]
