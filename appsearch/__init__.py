3505e983a85110a740cbcca9d68be92da9300bcd
Steven K <steven@pointcircle.com>
2021-05-28 14:12:23 -0700

    Bump

--- appsearch/__init__.py
+++ appsearch/__init__.py
@@ -3,9 +3,10 @@
 
 __name__ = "appsearch"
 __author__ = "Pivotal Energy Solutions"
-__version_info__ = (2, 1, 4)
+__version_info__ = (2, 1, 5)
 __version__ = ".".join(map(str, __version_info__))
 __date__ = "2014/07/22 4:47:00 PM"
+
 __credits__ = ["Tim Valenta", "Steven Klass"]
 __license__ = "See the file LICENSE.txt for licensing information."
 
