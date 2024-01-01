f2a6d88191e817615946f909914d3d108a08e895
Steven <rh0dium@users.noreply.github.com>
2024-01-01 08:30:35 -0700

    Updated Copyright 2024 (#11)

--- demo_app/users/views.py
+++ demo_app/users/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import get_user_model
 
 __author__ = "Artem Hruzd"
 __date__ = "08/07/2019 21:59"
-__copyright__ = "Copyright 2011-2020 Pivotal Energy Solutions. All rights reserved."
+__copyright__ = "Copyright 2011-2024 Pivotal Energy Solutions. All rights reserved."
 __credits__ = [
     "Artem Hruzd",
     "Steven Klass",
